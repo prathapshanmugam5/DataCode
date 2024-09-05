c0a1f688866c013e5beffc07fb53b1b54fe03909
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2024-09-05 18:01:48 +0530

    image upload added

---
+++ src/main/java/com/DATA/DataCodeAnalysing/Service/GitHubService.java
@@ -0,0 +1,11 @@
+package com.DATA.DataCodeAnalysing.Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface GitHubService {
+
+	public String uploadImage(MultipartFile file);
+
+}
